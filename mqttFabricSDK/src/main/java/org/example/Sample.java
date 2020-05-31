40bae1f771ba622aade8d0a77be69cc2b1b5eab7
Ali Alzubaidi <aakzubaidi@gmail.com>
2020-05-31 16:19:42 +0100

    fixing the connection between HLF sdk and network ( add static method jsut below class)

--- mqttFabricSDK/src/main/java/org/example/Sample.java
+++ mqttFabricSDK/src/main/java/org/example/Sample.java
@@ -10,44 +10,43 @@ import org.hyperledger.fabric.gateway.ContractException;
 import org.hyperledger.fabric.gateway.Gateway;
 import org.hyperledger.fabric.gateway.Network;
 import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric.sdk.identity.Identity;
 
 public final class Sample {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    static {
+		System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
+	}
+    public static void main(String[] args) throws IOException {
 
+        // Load an existing wallet holding identities used to access the network.
+        Path walletDirectory = Paths.get("Org1");
+        Wallet wallet = Wallet.createFileSystemWallet(walletDirectory);
 
-        Wallet fabricWallet;
-        Gateway.Builder builder;
-        Network network;
-        Contract contract;
-        String homedir = System.getProperty("user.home");
-        Path walletPath = Paths.get(homedir, ".fabric-vscode", "environments", "1 Org Local Fabric", "wallets", "Org1");
-        Path connectionProfilePath = Paths.get(homedir, ".fabric-vscode", "environments", "1 Org Local Fabric", "gateways", "Org1", "Org1.json");
-        String identityName = "admin";
-        
-        fabricWallet = Wallet.createFileSystemWallet(walletPath);
-        builder = Gateway.createBuilder();
-        builder.identity(fabricWallet, identityName).networkConfig(connectionProfilePath).discovery(true);
-    
+        // Path to a common connection profile describing the network.
+        Path networkConfigFile = Paths.get("1 Org Local Fabric - Org1_connection.json");
+
+        // Configure the gateway connection used to access the network.
+        Gateway.Builder builder = Gateway.createBuilder()
+                .identity(wallet, "org1Admin")
+                .networkConfig(networkConfigFile).discovery(true);
+
         // Create a gateway connection
-        
         try (Gateway gateway = builder.connect()) {
 
-            
             // Obtain a smart contract deployed on the network.
-             network = gateway.getNetwork("mychannel");
-             contract = network.getContract("QoSenforce", "QoSContract");
-            // Submit transactions that creates a record state for the performance of a cloud provider
-            //byte[] CreateCloudProviderRecord = contract.submitTransaction("createMonitoringReport", "CP1", "0", "0");
-            //System.out.println(new String(CreateCloudProviderRecord, StandardCharsets.UTF_8));
-            // a monitoring agents submits incident to the smart contract about a cloud provider
-            byte[] ReportViolationResult = contract.submitTransaction("reportViolation", "cp1", "3", "1000");
-            System.out.println(new String(ReportViolationResult, StandardCharsets.UTF_8));
+            Network network = gateway.getNetwork("mychannel");
+            Contract contract = network.getContract("Ali");
 
-            // Evaluate transactions that query state from the ledger.
-            //byte[] queryAllCarsResult = contract.evaluateTransaction("queryAllCars");
-            //System.out.println(new String(queryAllCarsResult, StandardCharsets.UTF_8));
+            byte[] CreateCloudProviderRecord = contract.submitTransaction("createMonitoringReport", "cp1", "0", "0");
+            System.out.println(new String(CreateCloudProviderRecord, StandardCharsets.UTF_8));
+            // a monitoring agents submits incident to the smart contract about a cloud provider
+            //byte[] ReportViolationResult = contract.submitTransaction("reportViolation", "cp1", "3", "1000");
+            //System.out.println(new String(ReportViolationResult, StandardCharsets.UTF_8));
 
         } catch (ContractException | TimeoutException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
