1854dfc9a5576efa93dc4209f432f65f89f9d6ad
Ali Alzubaidi <aakzubaidi@gmail.com>
2020-06-01 22:32:41 +0100

    Rearranging classes of MQTT monitor and Fabric SDK

--- mqttFabricSDK/src/main/java/org/example/FabricSDK.java
+++ mqttFabricSDK/src/main/java/org/example/FabricSDK.java
@@ -10,13 +10,13 @@ import org.hyperledger.fabric.gateway.ContractException;
 import org.hyperledger.fabric.gateway.Gateway;
 import org.hyperledger.fabric.gateway.Network;
 import org.hyperledger.fabric.gateway.Wallet;
-import org.hyperledger.fabric.sdk.identity.Identity;
 
-public final class Sample {
+public final class FabricSDK {
     static {
 		System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
-	}
-    public static void main(String[] args) throws IOException {
+    }
+    
+    String reportIncident (String mqttFailure) throws IOException {
 
         // Load an existing wallet holding identities used to access the network.
         Path walletDirectory = Paths.get("Org1");
@@ -37,17 +37,18 @@ public final class Sample {
             Network network = gateway.getNetwork("mychannel");
             Contract contract = network.getContract("Ali");
 
-            byte[] CreateCloudProviderRecord = contract.submitTransaction("createMonitoringReport", "cp1", "0", "0");
-            System.out.println(new String(CreateCloudProviderRecord, StandardCharsets.UTF_8));
-            // a monitoring agents submits incident to the smart contract about a cloud provider
-            //byte[] ReportViolationResult = contract.submitTransaction("reportViolation", "cp1", "3", "1000");
-            //System.out.println(new String(ReportViolationResult, StandardCharsets.UTF_8));
+            //byte[] CreateCloudProviderRecord = contract.submitTransaction("createMonitoringReport", "cp1", "0", "0");
+            //System.out.println(new String(CreateCloudProviderRecord, StandardCharsets.UTF_8));
+             //a monitoring agents submits incident to the smart contract about a cloud provider
+            byte[] ReportViolationResult = contract.submitTransaction("reportViolation", "cp1", "3", "1000");
+            System.out.println(new String(ReportViolationResult, StandardCharsets.UTF_8));
 
         } catch (ContractException | TimeoutException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
+        return ("");
     }
 }
